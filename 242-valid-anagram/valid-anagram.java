c90e7d2ba8b930371a63de2f898759cdb7a6531c
Ashish10102006 <ashishpanda2006.10@gmail.com>
2026-03-09 19:36:49 +0530

    Add anagram checking functionality
    Implemented an anagram checker using HashMap.

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -20,4 +20,36 @@ class Solution {
 
         return true;
     }
+}    
+
+2nd Way Using Array
+    import java.util.HashMap;
+
+public class ValidAnagram {
+    public static boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : t.toCharArray()) {
+            if (!map.containsKey(c) || map.get(c) == 0) {
+                return false;
+            }
+            map.put(c, map.get(c) - 1);
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("listen", "silent")); // true
+        System.out.println(isAnagram("rat", "car")); // false
+    }
 }
