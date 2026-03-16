d5451a880e332676308458e9ec7a7ae9c40bf029
Ashish10102006 <ashishpanda2006.10@gmail.com>
2026-03-16 21:55:22 +0530

    Time: 4 ms (81.49%) | Memory: 46.4 MB (39.00%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -2,51 +2,31 @@ import java.util.HashMap;
 
 class Solution {
     public boolean isAnagram(String s, String t) {
+        // if (s.length() != t.length())
+        //     return false;
+
+        // HashMap<Character, Integer> map = new HashMap<>();
+
+        // for (char c : s.toCharArray()) {
+        //     map.put(c, map.getOrDefault(c, 0) + 1);
+        // }
+
+        // for (char c : t.toCharArray()) {
+        //     if (!map.containsKey(c) || map.get(c) == 0) {
+        //         return false;
+        //     }
+        //     map.put(c, map.get(c) - 1);
+        // }
+
+        // return true;
+
+//2nd Way to Solve 
         if (s.length() != t.length())
-            return false;
-
-        HashMap<Character, Integer> map = new HashMap<>();
-
-        for (char c : s.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
-        }
-
-        for (char c : t.toCharArray()) {
-            if (!map.containsKey(c) || map.get(c) == 0) {
-                return false;
-            }
-            map.put(c, map.get(c) - 1);
-        }
-
-        return true;
-    }
-}    
-
-2nd Way Using Array...............................
-    public class AnagramCheck {
-    public static boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
-
-        int[] count = new int[26];
-
-        for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i) - 'a']++;
-            count[t.charAt(i) - 'a']--;
-        }
-
-        for (int c : count) {
-            if (c != 0) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(isAnagram("listen", "silent")); // true
-        System.out.println(isAnagram("rat", "car")); // false
+           return false;
+        char a1[]=s.toCharArray();
+        char a2[]=t.toCharArray();
+        Arrays.sort(a1);
+        Arrays.sort(a2);
+        return Arrays.equals(a1,a2);
     }
 }
