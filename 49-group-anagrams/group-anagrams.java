a3f3afa22153fe9e8dca0dd3253fa41c841a84fb
Ashish10102006 <ashishpanda2006.10@gmail.com>
2026-03-22 21:20:45 +0530

    Time: 7 ms (75.54%) | Memory: 49.5 MB (90.17%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,39 +1,59 @@
-import java.util.*;
+// import java.util.*;
+
+// class Solution {
+//     public List<List<String>> groupAnagrams(String[] strs) {
+//         List<List<String>> result = new ArrayList<>();
+//         boolean[] visited = new boolean[strs.length];
+
+//         for (int i = 0; i < strs.length; i++) {
+//             if (visited[i]) continue;
+
+//             List<String> group = new ArrayList<>();
+//             group.add(strs[i]);
+//             visited[i] = true;
+
+//             for (int j = i + 1; j < strs.length; j++) {
+//                 if (!visited[j] && isAnagram(strs[i], strs[j])) {
+//                     group.add(strs[j]);
+//                     visited[j] = true;
+//                 }
+//             }
+
+//             result.add(group);
+//         }
+
+//         return result;
+//     }
+
+//     private boolean isAnagram(String s1, String s2) {
+//         if (s1.length() != s2.length()) return false;
+
+//         char[] a = s1.toCharArray();
+//         char[] b = s2.toCharArray();
+
+//         Arrays.sort(a);
+//         Arrays.sort(b);
+
+//         return Arrays.equals(a, b);
+//     }
+// }   
+// ................Brute Force..................................
 
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> result = new ArrayList<>();
-        boolean[] visited = new boolean[strs.length];
-
-        for (int i = 0; i < strs.length; i++) {
-            if (visited[i]) continue;
-
-            List<String> group = new ArrayList<>();
-            group.add(strs[i]);
-            visited[i] = true;
-
-            for (int j = i + 1; j < strs.length; j++) {
-                if (!visited[j] && isAnagram(strs[i], strs[j])) {
-                    group.add(strs[j]);
-                    visited[j] = true;
-                }
-            }
-
-            result.add(group);
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String word : strs) {
+            // Sort the characters of the word to form the key
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            
+            // If key doesn't exist, create new list
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(word);
         }
-
-        return result;
-    }
-
-    private boolean isAnagram(String s1, String s2) {
-        if (s1.length() != s2.length()) return false;
-
-        char[] a = s1.toCharArray();
-        char[] b = s2.toCharArray();
-
-        Arrays.sort(a);
-        Arrays.sort(b);
-
-        return Arrays.equals(a, b);
+        
+        return new ArrayList<>(map.values());
     }
 }
