744cb4a6f1a9c7615db92de583fd9f846a79aa05
Ashish10102006 <ashishpanda2006.10@gmail.com>
2026-06-13 11:10:53 +0530

    Time: 2 ms (96.31%) | Memory: 48.6 MB (11.47%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,15 +1,33 @@
+// class Solution {
+//     public int[] twoSum(int[] numbers, int target) {
+//         int ar[]=new int[2];
+//         for(int i=0;i<numbers.length-1;i++){
+//             for(int j=i+1;j<numbers.length;j++){
+//                 if(numbers[i]+numbers[j]==target){
+//                     ar[0] = i + 1;
+//                     ar[1] = j + 1;
+//                     return ar;
+//                 }
+//             }
+//         }
+//         return ar;
+//     }
+// }
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int ar[]=new int[2];
-        for(int i=0;i<numbers.length-1;i++){
-            for(int j=i+1;j<numbers.length;j++){
-                if(numbers[i]+numbers[j]==target){
-                    ar[0] = i + 1;
-                    ar[1] = j + 1;
-                    return ar;
-                }
+        int i=0;int j=numbers.length-1;
+        while(i<j){
+            int sum=numbers[i]+numbers[j];
+            if(sum==target){
+                return new int[]{i+1,j+1};
+            }
+            else if(sum<target){
+                i++;
+            }
+            else{
+                j--;
             }
         }
-        return ar;
+        return new int[]{};
     }
 }
