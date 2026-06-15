56dac89b23f87a88d64847c7d6273b5c832a775d
Ashish10102006 <ashishpanda2006.10@gmail.com>
2026-06-15 12:41:36 +0530

    Time: 2 ms (13.72%) | Memory: 42.5 MB (86.48%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String words[]=s.split("\\s+");
+        if(words.length != pattern.length()) return false;
+        Map<Character,String> map= new HashMap<>();
+        Set<String> usedWords = new HashSet<>();
+        for(int i=0;i<pattern.length();i++){
+            char c= pattern.charAt(i);
+            String word=words[i];
+            if(map.containsKey(c)){
+                if(!map.get(c).equals(word)) return false;
+            }
+            else {
+                if (usedWords.contains(word)) {
+                    return false;
+                }
+
+                map.put(c, word);
+                usedWords.add(word);
+            }
+        }
+        return true;
+    }
+}
