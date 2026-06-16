70b721d6f041012328d09b781d699ddd652e0271
Ashish10102006 <ashishpanda2006.10@gmail.com>
2026-06-16 11:28:41 +0530

    Time: 1 ms (79.74%) | Memory: 42.9 MB (5.45%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,20 +1,46 @@
+// class Solution {
+//     public boolean isHappy(int n) {
+//         Set<Integer> set = new HashSet<>();
+//         while(n!=1){
+//             if(set.contains(n)) return false;
+//             set.add(n);
+//             n=sumOfSquare(n);
+//         }
+//         return true;
+//     }
+//     public int sumOfSquare(int n){
+//         int sum=0;
+//         while(n!=0){
+//             int digit=n%10;
+//             sum+=digit*digit;
+//             n/=10;
+//         }
+//         return sum;
+//     }
+// }
 class Solution {
-    public boolean isHappy(int n) {
-        Set<Integer> set = new HashSet<>();
-        while(n!=1){
-            if(set.contains(n)) return false;
-            set.add(n);
-            n=sumOfSquare(n);
-        }
-        return true;
-    }
-    public int sumOfSquare(int n){
-        int sum=0;
-        while(n!=0){
-            int digit=n%10;
-            sum+=digit*digit;
-            n/=10;
+    public int getNext(int n){
+        int sum = 0;
+        while(n != 0){
+            int digit = n % 10;
+            n = n / 10;
+
+            sum += Math.pow(digit,2);
         }
         return sum;
     }
+
+    public boolean isHappy(int n) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        while(true){
+            //add element to map
+            map.put(n,0);
+            n = getNext(n); //update next
+
+            //now check whether n is in map or n is 1 
+            if(n == 1)  return true;
+            if(map.containsKey(n))  return false;
+        }
+    }   
 }
