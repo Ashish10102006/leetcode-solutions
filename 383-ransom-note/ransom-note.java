fd1be2ab5f8c25e4d0be81cbadf00640b9fa2311
Ashish10102006 <ashishpanda2006.10@gmail.com>
2026-06-16 11:14:08 +0530

    Time: 15 ms (30.82%) | Memory: 47.5 MB (7.09%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if(magazine.length() < ransomNote.length())
+        return false;
+
+        Map<Character,Integer> map= new HashMap<>();
+        for(char c:magazine.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+
+        for(char c:ransomNote.toCharArray()){
+            if(!map.containsKey(c) || map.get(c)==0){
+                return false;
+            }
+            else{
+                map.put(c,map.get(c)-1);
+            }
+        }
+        return true;
+    }
+}
