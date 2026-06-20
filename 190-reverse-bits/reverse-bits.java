fb8936eb921ce8b79f093003cb3796ed15311df0
Ashish10102006 <ashishpanda2006.10@gmail.com>
2026-06-20 11:49:05 +0530

    Time: 1 ms (60.15%) | Memory: 42.4 MB (51.10%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -1,41 +1,41 @@
-class Solution {
-    public int reverseBits(int n) {
+// class Solution {
+//     public int reverseBits(int n) {
 
-        String binary_of_n = decimalToBinary(n);
+//         String binary_of_n = decimalToBinary(n);
 
-        // ensure 32 bits
-        while (binary_of_n.length() < 32) {
-            binary_of_n = "0" + binary_of_n;
-        }
+//         // ensure 32 bits
+//         while (binary_of_n.length() < 32) {
+//             binary_of_n = "0" + binary_of_n;
+//         }
 
-        String reversed = new StringBuilder(binary_of_n).reverse().toString();
+//         String reversed = new StringBuilder(binary_of_n).reverse().toString();
 
-        return binaryToDecimal(reversed);
-    }
+//         return binaryToDecimal(reversed);
+//     }
 
-    public String decimalToBinary(int n) {
-        if (n == 0) return "0";
+//     public String decimalToBinary(int n) {
+//         if (n == 0) return "0";
 
-        StringBuilder sb = new StringBuilder();
+//         StringBuilder sb = new StringBuilder();
 
-        while (n > 0) {
-            sb.append(n % 2);
-            n = n / 2;
-        }
+//         while (n > 0) {
+//             sb.append(n % 2);
+//             n = n / 2;
+//         }
 
-        return sb.reverse().toString();
-    }
+//         return sb.reverse().toString();
+//     }
 
-    public int binaryToDecimal(String binary) {
-        int result = 0;
+//     public int binaryToDecimal(String binary) {
+//         int result = 0;
 
-        for (int i = 0; i < binary.length(); i++) {
-            result = result * 2 + (binary.charAt(i) - '0');
-        }
+//         for (int i = 0; i < binary.length(); i++) {
+//             result = result * 2 + (binary.charAt(i) - '0');
+//         }
 
-        return result;
-    }
-}
+//         return result;
+//     }
+// }
 
 // 🧾 Binary → Decimal (Simple Note)
 // ✅ Rule:
@@ -51,3 +51,16 @@ class Solution {
 // 0 × 10 + 1 = 1
 // 1 × 10 + 2 = 12
 // 12 × 10 + 3 = 123
+
+ class Solution {
+    public int reverseBits(int n) {
+        int result=0;
+        for(int i=0;i<32;i++){
+            int lsb=n&1;
+            int reverse_lsb=lsb << (31-i);
+            result=result | reverse_lsb;
+            n=n >>1;
+        }
+        return result;
+    }
+}
