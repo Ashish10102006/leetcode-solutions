0dfda40ba8d1b108f919c6dc38cc01622aa73204
Ashish10102006 <ashishpanda2006.10@gmail.com>
2026-06-26 11:18:31 +0530

    Time: 4 ms (23.21%) | Memory: 47.5 MB (67.85%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,39 @@
+// Best Approach for Candy Problem
+// 👉 Use Two-pass Greedy
+
+// 🧠 Core Idea
+// You cannot decide candies in one direction because:
+// Left neighbor affects right
+// Right neighbor affects left
+// So you scan twice.
+
+class Solution {
+    public int candy(int[] ratings) {
+        int n=ratings.length;
+        int candies[]=new int[n];
+        Arrays.fill(candies,1);//Each child must have at least one candy.
+
+        // left to right
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]){
+                candies[i]=candies[i-1]+1;
+            }
+        }
+
+        // right to left
+        for(int i=n-2;i>=0;i--){
+            if (ratings[i] > ratings[i + 1]) {
+            candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+        int sum = 0;
+        for (int c : candies) sum += c;
+        return sum;
+    }
+}
+
+// Why max()?
+// Because:
+// Left pass might already give a good value
+// Right pass might require bigger value
+// You always keep the better (larger) requirement
