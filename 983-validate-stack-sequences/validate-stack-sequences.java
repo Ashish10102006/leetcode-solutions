d8fa70f0bedd5a8ccbf04bc390c8953b63034d98
Ashish10102006 <ashishpanda2006.10@gmail.com>
2026-07-07 13:23:27 +0530

    Time: 2 ms (90.49%) | Memory: 46.5 MB (25.93%) - LeetSync

--- 983-validate-stack-sequences/validate-stack-sequences.java
+++ 983-validate-stack-sequences/validate-stack-sequences.java
@@ -1,8 +1,9 @@
+// Push every element. After each push, keep popping as long as the stack's top matches the next required element in popped.
 class Solution {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
         Stack<Integer> stack=new Stack<>();
         int j=0;// Points to the next element we need to pop
-        
+
         for (int num : pushed) {
             // Push the current element
             stack.push(num);
