8400c4b06be0a2ba56e21869ca5d3c9cf745bc83
Ashish10102006 <ashishpanda2006.10@gmail.com>
2026-07-09 11:59:43 +0530

    Time: 55 ms (50.00%) | Memory: 48.3 MB (55.86%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
@@ -1,5 +1,5 @@
 import java.util.*;
-
+/*
 class Solution {
     public String sortVowels(String s) {
         PriorityQueue<Character> pq = new PriorityQueue<>();
@@ -18,6 +18,42 @@ class Solution {
         // Replace vowels with sorted vowels
         for (int index : indices) {
             arr[index] = pq.poll();
+        }
+
+        return new String(arr);
+    }
+
+    private boolean isVowel(char ch) {
+        return "AEIOUaeiou".indexOf(ch) != -1;
+    }
+}
+*/
+
+// 2nd Approch
+import java.util.*;
+
+class Solution {
+    public String sortVowels(String s) {
+        List<Character> vowels = new ArrayList<>();
+
+        // Extract vowels
+        for (char ch : s.toCharArray()) {
+            if (isVowel(ch)) {
+                vowels.add(ch);
+            }
+        }
+
+        // Sort vowels by ASCII
+        Collections.sort(vowels);
+
+        char[] arr = s.toCharArray();
+        int j = 0;
+
+        // Put sorted vowels back
+        for (int i = 0; i < arr.length; i++) {
+            if (isVowel(arr[i])) {
+                arr[i] = vowels.get(j++);
+            }
         }
 
         return new String(arr);
