af9e56426e81ee55758da3f1a16203f7b5a4274b
Ashish10102006 <ashishpanda2006.10@gmail.com>
2026-07-16 12:36:10 +0530

    Time: 1 ms (100.00%) | Memory: 47.2 MB (76.75%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public int[] sortedSquares(int[] nums) {
         int result[]=new int[nums.length];
@@ -5,6 +6,33 @@ class Solution {
             result[i]=nums[i]*nums[i];
         }
         Arrays.sort(result);
+        return result;
+    }
+}
+*/
+
+//2=ponter approch
+// "Since the array is sorted, the largest absolute value is always at one of the two ends. Therefore, I compare the left and right ends, place the larger square at the end of the result array, and move the corresponding pointer. If the array were not sorted, this property would not hold, so the two-pointer approach would not work."
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        int left = 0;
+        int right = n - 1;
+        int index = n - 1;
+
+        while (left <= right) {
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                result[index] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[index] = nums[right] * nums[right];
+                right--;
+            }
+            index--;
+        }
+
         return result;
     }
 }
