90c4ed2d9ad3a3ead26c31ce8cf34c19824a2805
Ashish10102006 <ashishpanda2006.10@gmail.com>
2026-07-18 11:37:24 +0530

    Time: 21 ms (78.06%) | Memory: 105.2 MB (10.68%) - LeetSync

--- 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.java
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public String addSpaces(String s, int[] spaces) {
         StringBuilder str = new StringBuilder(s);
@@ -11,3 +12,19 @@ class Solution {
         return str.toString();
     }
 }
+//offset means how many extra characters (spaces) have already been inserted.
+*/
+class Solution {
+    public String addSpaces(String s, int[] spaces) {
+      StringBuilder result=new StringBuilder();
+      int prev=0;
+      for(int space:spaces) {
+        result.append(s.substring(prev,space));
+        result.append(' ');
+        prev=space;
+      
+      } 
+      result.append(s.substring(prev));
+      return result.toString();
+    }
+}    
