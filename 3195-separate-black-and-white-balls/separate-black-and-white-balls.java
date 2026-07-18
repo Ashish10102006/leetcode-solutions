475173d568e537b85d730d434847f3081845253b
Ashish10102006 <ashishpanda2006.10@gmail.com>
2026-07-18 12:29:06 +0530

    Time: 8 ms (52.68%) | Memory: 47.8 MB (19.15%) - LeetSync

---
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long minimumSteps(String s) {
+        int left=0;
+        int index=0;
+        long result=0;
+        while(index<s.length()){
+            if(s.charAt(index)=='0'){
+                result+=index-left;
+                left++;
+            }
+            index++;
+        }
+        return result;
+    }
+}
