54866c1c4724f421acafa9e4fba49224723dd17f
Ashish10102006 <ashishpanda2006.10@gmail.com>
2026-07-20 13:28:20 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (87.13%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -9,6 +9,9 @@
  *     }
  * }
  */
+
+ /* 
+ //Brutee fore approch
 public class Solution {
     public ListNode detectCycle(ListNode head) {
         HashSet<ListNode> set =new HashSet<>();
@@ -20,3 +23,31 @@ public class Solution {
         return null;
     }
 }
+*/
+//optimal approch
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast !=null && fast.next!=null)// and becoz fast is moving two steps at a time 
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+            
+            if(slow==fast){
+                while(head!=slow){
+                    head=head.next;
+                    slow=slow.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
+
+// Note for your notebook
+// Cycle detection: slow → 1 step, fast → 2 steps.
+// Finding cycle start: head → 1 step, slow → 1 step.
+// Reason: Moving both one step makes them meet at the first node of the cycle.
