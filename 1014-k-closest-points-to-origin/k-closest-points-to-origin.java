4f7d3115c662f3a4f0160a50cbb34463f6937c3d
Ashish10102006 <ashishpanda2006.10@gmail.com>
2026-07-31 14:52:51 +0530

    Time: 21 ms (94.23%) | Memory: 51.7 MB (93.62%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[0] * a[0] + a[1] * a[1],b[0] * b[0] + b[1] * b[1]));
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[0] * a[0] + a[1] * a[1] , b[0] * b[0] + b[1] * b[1]));
 
         for(int i=0;i<points.length;i++){
             pq.offer(points[i]);
         }
 
-        
+
         int[][] result = new int[k][2];
         for (int i = 0; i < k; i++) {
             result[i] = pq.poll();
@@ -15,3 +15,25 @@ class Solution {
             return result;
     }
 }
+/*
+
+Quick rule to remember:
+
+1st-Way:
+Ascending order (min-heap):
+Integer.compare(a, b)
+or
+a - b
+
+Descending order (max-heap):
+Integer.compare(b, a)
+or
+b - a
+
+< 0 → a first
+> 0 → b first
+0 → same priority
+
+2-Way:
+(a, b) -> valueOfA - valueOfB
+*/
