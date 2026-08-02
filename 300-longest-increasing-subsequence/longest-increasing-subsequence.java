ff6187316b092b6e9e18ccdf070c890f1f362988
Ashish10102006 <ashishpanda2006.10@gmail.com>
2026-08-02 13:58:03 +0530

    Time: 37 ms (72.15%) | Memory: 46.2 MB (56.87%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n=nums.length;
+        int[] dp = new int[n];
+        int maxLength = 1;
+
+          // Every element alone is an increasing subsequence of length 1
+        Arrays.fill(dp, 1); 
+
+         for (int i = 1; i < n; i++) {
+            for(int j=0; j<i; j++){
+                if(nums[j]<nums[i]) dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+            maxLength = Math.max(maxLength, dp[i]);
+         }
+         return maxLength;
+
+    }
+}
+/*
+// Meaning of dp
+dp[i] = Length of the Longest Increasing Subsequence ending at index i;
+*/
