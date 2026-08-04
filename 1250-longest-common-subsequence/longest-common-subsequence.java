8b8a119a2eab73c04c463b6b5eb35747597935fc
Ashish10102006 <ashishpanda2006.10@gmail.com>
2026-08-04 10:22:54 +0530

    Time: 24 ms (34.20%) | Memory: 53.7 MB (82.31%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,36 +1,32 @@
 class Solution {
-
     public int longestCommonSubsequence(String text1, String text2) {
 
-        Integer[][] dp = new Integer[text1.length()][text2.length()];
+        int n = text1.length();
+        int m = text2.length();
+        int dp[][] = new int[n+1][m+1];
+     
+        for(int i =0;i<n+1;i++){
+            for(int j=0;j<m+1;j++){
+               if(i==0||j==0){
+                dp[i][j]=0;
+               }
+            }
+        } 
 
-        return solve(text1, text2, 0, 0, dp);
-    }
 
-    private int solve(String s1, String s2, int i, int j, Integer[][] dp) {
+        for(int i =1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }
+               else{
+                int ans1 = dp[i-1][j];
+                int ans2 = dp[i][j-1];
 
-        // Base case
-        if (i == s1.length() || j == s2.length()) {
-            return 0;
-        }
-
-        // Already computed
-        if (dp[i][j] != null) {
-            return dp[i][j];
-        }
-
-        // Characters match
-        if (s1.charAt(i) == s2.charAt(j)) {
-            dp[i][j] = 1 + solve(s1, s2, i + 1, j + 1, dp);
-        }
-        // Characters don't match
-        else {
-            dp[i][j] = Math.max(
-                    solve(s1, s2, i + 1, j, dp),
-                    solve(s1, s2, i, j + 1, dp)
-            );
-        }
-
-        return dp[i][j];
+                dp[i][j]=Math.max(ans1,ans2);
+               }
+            }
+        } 
+         return dp[n][m];
     }
 }
