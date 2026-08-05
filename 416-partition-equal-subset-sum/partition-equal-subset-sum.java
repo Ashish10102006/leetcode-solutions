5ecd5c414c6eedf84e5760d6ddf603825439da97
Ashish10102006 <ashishpanda2006.10@gmail.com>
2026-08-05 13:35:00 +0530

    Time: 23 ms (91.55%) | Memory: 72.9 MB (23.34%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -55,31 +55,19 @@ class Solution {
     }
 }
 /*
-/dp[s] = Can I make the sum s using the numbers I've processed so far?
-boolean[] dp = new boolean[target + 1];
-But here's the important part.
-The DP array is not checking the total sum.
+dp[index][target]
+read it as:
+"Starting from index, can I make the remaining target?"
 
-Think of it like this
-Suppose:
-nums = [1, 5, 11, 5]
-target = 11
-After processing only 1, the possible sums are:
-0 ✓
-1 ✓
-2 ✗
-3 ✗
-...
-11 ✗
 
-After processing 5, the possible sums become:
-0 ✓
-1 ✓
-5 ✓
-6 ✓
-After processing 11:
-11 ✓
-As soon as dp[11] becomes true, you know:
-Yes, there exists a subset whose sum is 11.
-Since 11 is half of the total (22), the remaining elements also sum to 11, so the array can be partitioned into two equal subsets.
+Remember this one sentence:
+boolean = 2 states (true, false)
+Boolean = 3 states (true, false, null)
+So every cell can have three values:
+
+Value	Meaning
+null	Not solved yet
+true	Solved, answer is yes
+false	Solved, answer is no
+
 */
