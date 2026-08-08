17bcbc05e7c150c024b6cc5f389febe62420a94b
Ashish10102006 <ashishpanda2006.10@gmail.com>
2026-08-08 11:41:02 +0530

    Time: 3 ms (87.47%) | Memory: 52.2 MB (59.81%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.java
@@ -0,0 +1,39 @@
+/*
+"I'll scan every cell. When I find land that I haven't deal with yet, I'll explore all land connected to it. That gives me one island."
+*/
+class Solution {
+    public int numIslands(char[][] grid) {
+        int islands=0;
+        for( int i=0 ;i< grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]=='1'){
+                    islands++;
+                    dfs(grid,i,j);
+                }
+            }
+        }
+        return islands;
+    }
+    private void dfs(char[][] grid, int row, int col) {
+
+        // Outside the grid
+        if (row < 0 || row >= grid.length ||
+            col < 0 || col >= grid[0].length) {
+            return;
+        }
+
+        // Water or already visited land
+        if (grid[row][col] == '0') {
+            return;
+        }
+
+        // Mark as visited
+        grid[row][col] = '0';
+
+        // Explore 4 directions
+        dfs(grid, row - 1, col); // up
+        dfs(grid, row + 1, col); // down
+        dfs(grid, row, col - 1); // left
+        dfs(grid, row, col + 1); // right
+    }
+}
