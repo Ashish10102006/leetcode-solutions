2e164ec74ca266c2ab44df8e96ff089c3f42f4f7
Ashish10102006 <ashishpanda2006.10@gmail.com>
2026-08-28 17:23:50 +0530

    Time: 3 ms (72.83%) | Memory: 52.6 MB (40.79%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,3 +1,5 @@
+// 1st way=hashmap
+/*
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n=nums.length;
@@ -21,3 +23,66 @@ More than n/3 → maximum 2 elements
 
 More than n/k → maximum k-1 elements
 */
+
+//2nd way =moores vooting process
+
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int n=nums.length;
+
+        int candidate1=0;
+        int candidate2=0;
+
+        int count1=0;
+        int count2=0;
+
+
+        // Step 1: Find possible candidates
+        for(int i:nums){
+            if (count1 == 0 && i != candidate2) {
+                candidate1 = i;
+            }
+            else if (count2 == 0 && i != candidate1) {
+                candidate2 = i;
+            }
+
+            if (i == candidate1) {
+                count1++;
+            }
+            else if (i == candidate2) {
+                count2++;
+            }
+            else {
+                count1--;
+                count2--;
+            }    
+        }
+
+        // Step 2: Verify candidates
+        count1=0;
+        count2=0;
+
+        for (int i : nums) {
+            if (i == candidate1) {
+                count1++;
+            }
+            else if (i == candidate2) {
+                count2++;
+            }
+        }
+
+        // Step 3: Add valid candidates
+        List<Integer> result = new ArrayList<>();
+
+        if (count1 > n / 3) {
+            result.add(candidate1);
+        }
+
+        if (count2 > n / 3) {
+            result.add(candidate2);
+        }
+
+        return result;
+
+    }
+}        
