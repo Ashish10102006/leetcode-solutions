02b375768258504cbf9194592b123db86ba1b9d0
Ashish10102006 <ashishpanda2006.10@gmail.com>
2026-08-31 10:49:37 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (19.61%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public int findPeakElement(int[] nums) {
         int n=nums.length;
@@ -5,7 +6,7 @@ class Solution {
         if(n == 1) {
             return 0;
         }
-        
+
         for(int i=0;i<n;i++){
             if(i==n-1){
                 if(nums[i]>nums[i-1]) return i; 
@@ -21,3 +22,39 @@ class Solution {
         return -1;
     }
 }
+*/
+
+//2ND WAY BINARY SEARCH
+
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] > nums[mid + 1]) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+}
+
+/*
+                 Compare mid and mid+1
+                          ↓
+                ┌─────────┴─────────┐
+                ↓                   ↓
+          mid > mid+1          mid < mid+1
+             going DOWN           going UP
+                ↓                   ↓
+          peak is LEFT          peak is RIGHT
+                ↓                   ↓
+            high = mid        low = mid+1
+
+*/            
