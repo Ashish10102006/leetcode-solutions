e5229120f371e45774c85de92db6b1db967f3e7c
Ashish10102006 <ashishpanda2006.10@gmail.com>
2026-09-09 10:41:26 +0530

    Time: 1 ms (99.36%) | Memory: 42.6 MB (71.15%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -11,3 +11,51 @@ class Solution {
         return count;
     }
 }
+/*
+power means:
+
+power = 1000
+
+Count numbers having the 1st comma.
+
+Then:
+
+power = 1,000,000
+
+Count numbers having the 2nd comma.
+
+Then:
+
+power = 1,000,000,000
+
+Count numbers having the 3rd comma.
+
+And so on.
+
+The trick to discover these problems
+
+Don't immediately think about code.
+
+First make a table:
+
+Range                  Extra comma
+1000+                  1
+1,000,000+             2
+1,000,000,000+         3
+1,000,000,000,000+     4
+
+Then ask:
+
+"How many numbers are in each range?"
+
+That gives:
+
+n - 1000 + 1
+n - 1000000 + 1
+n - 1000000000 + 1
+...
+
+which simplifies to:
+
+n - power + 1
+*/
